0c0b7c6cd230a4435887cb99ec540fc4c726386f
hanranti <hanranti@cs.helsinki.fi>
2020-09-24 15:01:58 +0300

    build

---
+++ build/precache-manifest.fb03fe605ce39665a16e0c13f86d7742.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7c9b807e382e139a4da23e4dfe7c3bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f4d1edd387cf22cabab",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9af68b89fbfe292cb182",
+    "url": "/static/js/2.c7d8749e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c7d8749e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f4d1edd387cf22cabab",
+    "url": "/static/js/main.5e722b9f.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
